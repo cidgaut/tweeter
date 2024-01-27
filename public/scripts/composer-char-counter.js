9f0d84bda4e27a10c45110e654a82bec793692b2
Cedric.gauthier <cidgaut@gmail.com>
2024-01-27 15:29:26 -0500

    fixed my new-tweets when looking at dev logs for character count not turning red when count is over 140. Turns out I never specified the styles directory in href. As for the red, there is a space when I add the invalid class. I will need to replace the class entirely to avoid this

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
   // --- our code goes here ---
-  $(".tweet-text").on("input", function() {
-    console.log(this.value);
+  //using # to grab specific tweet-text .will be applied to everyting in tweet-text
+  $("#tweet-text").on("input", function() {
+
+    //keep track of characters typed
+    const characterCount = this.value.length;
+    const maxCharacters = 140 - characterCount;
+    
+    //counter for maxcharacters add invalid class if over max
+    $(".counter").text(maxCharacters);
+
+    if(maxCharacters < 0) {
+      $(".counter").addClass("invalid");
+      console.log("invalid class added");
+    } else {
+      $(".counter").removeClass("invalid");
+      console.log("invalid class removed");
+    }
   });
 });
