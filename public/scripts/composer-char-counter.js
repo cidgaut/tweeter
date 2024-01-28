15c665dafce5dcd8692498f5fa4b7c69e9290223
Cedric.gauthier <cidgaut@gmail.com>
2024-01-28 00:15:49 -0500

    proper flex given to article. made article fit to content. added hover shadow-box and hover for each invididual icon

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
     const tweetForm = $(this).closest(".tweet-form");
 
     //find ".counter" elmnt within ".tweet-form"
-    const counter = tweetForm/FileSystemHandle(".counter");
+    const counter = tweetForm.find(".counter");
 
     //jQuery to turn elmnt into text, toggleClass that completely replaces class to invalid instead of adding.
     counter.text(maxCharacters).toggleClass("invalid", maxCharacters < 0);
